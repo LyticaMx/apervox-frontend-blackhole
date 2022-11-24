c2db6f402df4e5803595077fcf7da18080180200
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2022-11-24 17:00:17 -0600

    Se añaden las variables de entorno faltantes a la nueva configuración, se reemplazaron estos valores

--- src/providers/config/index.ts
+++ src/providers/config/index.ts
@@ -1,12 +1,16 @@
 export const config = {
   appConfig: {
     virtualizedTableOverscan:
-      parseInt(
-        process.env.REACT_APP_VIRTUALIZED_TABLE_OVERSCAN as string,
-        10
-      ) || 10
+      parseInt(process.env.REACT_APP_VIRTUALIZED_TABLE_OVERSCAN ?? '', 10) ||
+      10,
+    timePerTranscriptionPage:
+      parseInt(process.env.REACT_APP_TIME_PER_TRANSCRIPTION_PAGE ?? '', 10) ||
+      30,
+    itemsPerTranscriptionPage:
+      parseInt(process.env.REACT_APP_ITEMS_PER_TRANSCRIPTION_PAGE ?? '', 10) ||
+      9
   },
   endpoints: {
-    mainBackendUrl: process.env.REACT_APP_MAIN_BACKEND_URL
+    mainBackendUrl: process.env.REACT_APP_MAIN_BACKEND_URL ?? 'http://localhost'
   }
 }
