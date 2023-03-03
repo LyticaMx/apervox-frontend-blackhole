dbef30365e76539293f12e1fcf669c322d242756
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2023-03-02 18:10:41 -0600

    Se extrae el componente de filtros de imagen y se hacen las adecuaciones correspondientes en el editor de imagenes

--- src/components/ImageEditor/messages.ts
+++ src/components/ImageEditor/messages.ts
@@ -4,21 +4,24 @@ export const messages = defineMessages({
   negativeEffect: {
     id: 'components.ImageEditor.negativeEffect',
     defaultMessage: 'Aplicar efecto negativo'
-  },
+  }
+})
+
+export const imageFiltersMessages = defineMessages({
   contrast: {
-    id: 'components.ImageEditor.contrast',
+    id: 'components.ImageEditor.ImageFilters.contrast',
     defaultMessage: 'Contraste:'
   },
   hue: {
-    id: 'components.ImageEditor.hue',
+    id: 'components.ImageEditor.ImageFilters.hue',
     defaultMessage: 'Matiz:'
   },
   brightness: {
-    id: 'components.ImageEditor.brightness',
+    id: 'components.ImageEditor.ImageFilters.brightness',
     defaultMessage: 'Brillo:'
   },
   grayScale: {
-    id: 'components.ImageEditor.grayScale',
+    id: 'components.ImageEditor.ImageFilters.grayScale',
     defaultMessage: 'Color:'
   }
 })
