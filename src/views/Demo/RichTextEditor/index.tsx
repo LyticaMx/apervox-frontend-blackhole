0065f68a00fb16ce78cfe9479cd64500488e64e4
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2023-03-07 16:02:49 -0600

    Se agrega titulo a la vista de demo

--- src/views/Demo/RichTextEditor/index.tsx
+++ src/views/Demo/RichTextEditor/index.tsx
@@ -1,5 +1,6 @@
 import Button from 'components/Button'
 import RichTextEditorComponent from 'components/RichTextEditor'
+import Typography from 'components/Typography'
 import { ReactElement, useRef } from 'react'
 import { DecoupledEditor } from 'types/richTextEditor'
 import './RichTextEditor.css'
@@ -9,6 +10,9 @@ const RichTextEditor = (): ReactElement => {
 
   return (
     <div>
+      <Typography variant="title" style="bold" className="mb-4">
+        Editor de texto enriquecido
+      </Typography>
       <RichTextEditorComponent
         initialData="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit mi ac nisl congue, sed vestibulum sem varius. Aliquam dapibus quis lacus eu vehicula. Quisque efficitur libero vitae sodales dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer consequat, orci finibus scelerisque suscipit, magna urna pulvinar lacus, eu lacinia neque enim non tellus. Integer accumsan, ipsum eu sollicitudin gravida, felis purus dapibus erat, et luctus nisi ligula egestas mauris. Nam sed sagittis purus. Sed tempus in leo eu bibendum. Donec venenatis, ex quis congue posuere, felis arcu pharetra odio, in porta leo ligula vel nisl. Nulla ac est enim. Etiam at augue vel nisi placerat blandit."
         editorRef={editorRef}
