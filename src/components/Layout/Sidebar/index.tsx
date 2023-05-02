844194774e9053e2e973e7e78531b4a087eb0e28
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2023-05-02 17:21:34 -0600

    Se cambian estilos de hover al boton de mi cuenta

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -5,10 +5,14 @@ import Item from './components/Item'
 import { SignalIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import { useSidebar } from 'context/Sidebar'
+import Tooltip from 'components/Tooltip'
+import { useIntl } from 'react-intl'
+import { sidebarMessages } from 'globalMessages'
 
 const Sidebar = (): ReactElement => {
   const { pathname } = useLocation()
   const { open } = useSidebar()
+  const { formatMessage } = useIntl()
   const history = useHistory()
 
   return (
@@ -20,12 +24,23 @@ const Sidebar = (): ReactElement => {
       )}
     >
       <nav className="flex flex-col flex-1 space-y-2 items-center">
-        <a
-          onClick={() => history.push('/mi-cuenta')}
-          className="p-1 text-white focus:outline-nones transition-colors duration-200 rounded-lg bg-primary"
+        <Tooltip
+          content={formatMessage(sidebarMessages.myAccount)}
+          floatProps={{ offset: 10, arrow: true }}
+          classNames={{
+            panel:
+              'bg-secondary text-white py-1 px-2 rounded-md text-sm whitespace-nowrap',
+            arrow: 'absolute bg-white w-2 h-2 rounded-full bg-secondary'
+          }}
+          placement="right"
         >
-          <UserCircleIcon className="h-5 w-5" />
-        </a>
+          <a
+            onClick={() => history.push('/mi-cuenta')}
+            className="p-1 text-white focus:outline-nones transition-colors duration-200 rounded-lg bg-primary cursor-pointer block hover:bg-primary-600"
+          >
+            <UserCircleIcon className="h-5 w-5" />
+          </a>
+        </Tooltip>
 
         {routes
           .filter((route) => route.sidebar)
