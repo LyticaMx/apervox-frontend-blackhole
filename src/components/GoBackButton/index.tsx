19476924bb03aa760082b9cfc50d9a9482f6a59f
Alfredo Rafael González Rodríguez <argr.295530@gmail.com>
2023-05-16 12:11:22 -0600

    Se agrega funcionalidad para enviar string o funcion en el GoBackButton

--- src/components/GoBackButton/index.tsx
+++ src/components/GoBackButton/index.tsx
@@ -1,16 +1,36 @@
 import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline'
 import Tooltip from 'components/Tooltip'
 import { actionsMessages } from 'globalMessages'
-import { ReactElement } from 'react'
+import { ReactElement, ReactNode } from 'react'
 import { useIntl } from 'react-intl'
 import { Link } from 'react-router-dom'
 
 interface Props {
-  route: string
+  route: string | (() => void)
 }
 
 const GoBackButton = (props: Props): ReactElement => {
   const { formatMessage } = useIntl()
+
+  const render = (): ReactNode => {
+    switch (typeof props.route) {
+      case 'string':
+        return (
+          <Link to={props.route} className="">
+            <ArrowUturnLeftIcon className="w-5 h-5 text-secondary-gray mr-3" />
+          </Link>
+        )
+      case 'function':
+        return (
+          <button onClick={props.route}>
+            <ArrowUturnLeftIcon className="w-5 h-5 text-secondary-gray mr-3" />
+          </button>
+        )
+
+      default:
+        return null
+    }
+  }
 
   return (
     <div className="absolute top-2 right-4 xl:right-16 2xl:right-32">
@@ -24,9 +44,7 @@ const GoBackButton = (props: Props): ReactElement => {
         }}
         placement="top"
       >
-        <Link to={props.route} className="">
-          <ArrowUturnLeftIcon className="w-5 h-5 text-secondary-gray mr-3" />
-        </Link>
+        {render()}
       </Tooltip>
     </div>
   )
